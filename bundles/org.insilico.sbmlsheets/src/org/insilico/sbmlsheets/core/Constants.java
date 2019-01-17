540993734b7124662b4150ba4c6563b2903f773d
RobertDeibel <deibel.robert@googlemail.com>
2019-01-17 12:25:51 +0100

    added a 'use SBML' Button including file explorer

--- bundles/org.insilico.sbmlsheets/src/org/insilico/sbmlsheets/core/Constants.java
+++ bundles/org.insilico.sbmlsheets/src/org/insilico/sbmlsheets/core/Constants.java
@@ -1,5 +1,26 @@
 package org.insilico.sbmlsheets.core;
 
 public interface Constants {
-
+	public static final String FUNCTION_DEF_TABLE = "Function Definitions";
+	
+	public static final String UNIT_DEF_TABLE = "Unit Definitions";
+	
+	public static final String COMPARTMENT_TABLE = "Compartments";
+	
+	public static final String SPECIES_TABLE = "Species";
+	
+	public static final String PARAMETERS_TABLE = "Parameters";
+	
+	public static final String INIT_ASSIGNMENTS_TABLE = "Initial Assignments";
+	
+	public static final String RULES_TABLE = "Rules";
+	
+	public static final String CONSTRAINTS_TABLE = "Constraints";
+	
+	public static final String REACTIONS_TABLE = "Reactions";
+	
+	public static final String EVENTS_TABLE = "Events";
+	
+	public static final String[] TABLE_TYPES = {FUNCTION_DEF_TABLE, UNIT_DEF_TABLE, COMPARTMENT_TABLE, SPECIES_TABLE, 
+			PARAMETERS_TABLE, INIT_ASSIGNMENTS_TABLE, RULES_TABLE, CONSTRAINTS_TABLE, REACTIONS_TABLE, EVENTS_TABLE};
 }
--- bundles/org.insilico.sbmlsheets/src/org/insilico/sbmlsheets/editor/SheetProjectView.java
+++ bundles/org.insilico.sbmlsheets/src/org/insilico/sbmlsheets/editor/SheetProjectView.java
@@ -7,7 +7,13 @@ import java.io.File;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.swing.plaf.synth.SynthSpinnerUI;
 
+import org.eclipse.e4.core.contexts.Active;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.basic.MTrimmedWindow;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.insilico.sbmlsheets.core.SheetProject;
 import org.insilico.sbmlsheets.core.SheetWriter;
 
@@ -38,6 +44,7 @@ import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextBoundsType;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Popup;
 import javafx.stage.Screen;
@@ -59,6 +66,9 @@ public class SheetProjectView {
 	@Inject
 	SheetProject project;
 	
+	@Inject 
+	MTrimmedWindow mWindow;
+	
 	ObservableList<String> pathsInDir;
 	/**
 	 * Called after injection of {@link #project}. Generates the GUI.
@@ -71,9 +81,9 @@ public class SheetProjectView {
 	}
 	
 	/**
-	 * 
-	 * @param parent
-	 * @return
+	 * Builds the View of the {@link SheetProject} in {@link #project}.
+	 * @param parent The parent {@link BorderPane} {@link Node} provided by Insilico
+	 * @return The View representing {@link #project}.
 	 */
 	private BorderPane makeView(BorderPane parent) {
 		
@@ -83,35 +93,59 @@ public class SheetProjectView {
 		ButtonBar bottomButtonBar = new ButtonBar();
 		//placing ButtonBar at bottom of screen
 		parent.setBottom(bottomButtonBar);
+		//Button for compilation of an SBML project
 		Button compileButton = new Button("_Compile");
+		//enable fireing of button through Alt+C
 		compileButton.setMnemonicParsing(true);
 		compileButton.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("Compiling...");
+				System.out.print("Compiling...");
 				project.compileSBML();
+				System.out.println("Done");
 			}
 		});
-		Button saveButton = new Button("Save");
+		//Button for saving the SheetsProject file to .sheets
+		Button saveButton = new Button("_Save");
+		saveButton.setMnemonicParsing(true);
 		saveButton.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("Syving...");
+				System.out.print("Saving...");
 				project.save();
+				System.out.println("Done");
 			}
 		});
-		bottomButtonBar.getButtons().addAll(compileButton, saveButton);
+		//Button for reading an SBML file and loading the Sheets/csv files accordingly
+		Button addSBMLButton = new Button("_Read from SBML");
+		addSBMLButton.setMnemonicParsing(true);
+		addSBMLButton.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.print("Reading SBML file...");
+				choseSBMLfile();
+				System.out.println("Done");
+			}
+			
+			
+
+		});
+		bottomButtonBar.getButtons().addAll(addSBMLButton, saveButton, compileButton);
 		
+		//main area is a gridpane
 		GridPane gp = new GridPane();
 				
 		parent.setCenter(gp);
+		//Text
 		Text sbmlSpecificationText = new Text("SBML Specification:");
 		sbmlSpecificationText.setTextAlignment(TextAlignment.RIGHT);
 		sbmlSpecificationText.setBoundsType(TextBoundsType.LOGICAL);
+		//needed to not look like shit
 		sbmlSpecificationText.getStyleClass().add("text-id");
-		
+		//the SBML specification is defined here
 		TextField sbmlSpecification = new TextField(project.getSpecification());
 		sbmlSpecification.setPromptText("Enter SBML specification");
 		sbmlSpecification.setAlignment(Pos.CENTER);
@@ -123,7 +157,7 @@ public class SheetProjectView {
 				gp.requestFocus();
 			}
 		});
-		
+		//Button to set SBML specification
 		Button sbmlSpecificationChangeButton = new Button("Change");
 		sbmlSpecificationChangeButton.setOnAction(new EventHandler<ActionEvent>() {
 			
@@ -133,15 +167,21 @@ public class SheetProjectView {
 				gp.requestFocus();
 			}
 		});
+		//HBox combining the SBML specification controls: Button & TextField
 		HBox sbmlSpecificationControls = new HBox();
 		sbmlSpecificationControls.getChildren().addAll(sbmlSpecification, sbmlSpecificationChangeButton);
+		//add to GridPane
 		gp.add(sbmlSpecificationText, 0, 0);
 		gp.add(sbmlSpecificationControls, 1, 0);
 		
+		//More text
 		Text sbmlFileNameText = new Text("SBML File Name:");
 		sbmlFileNameText.setTextAlignment(TextAlignment.RIGHT);
 		sbmlFileNameText.setBoundsType(TextBoundsType.LOGICAL);
+		//not look like shit
 		sbmlFileNameText.getStyleClass().add("text-id");
+		
+		//the SBML file name is defined here
 		TextField sbmlFileName = new TextField(project.getSbmlFileName());
 		sbmlFileName.setPromptText("Enter SBML file name");
 		sbmlFileName.setAlignment(Pos.CENTER);
@@ -153,7 +193,7 @@ public class SheetProjectView {
 				gp.requestFocus();
 			}
 		});
-		
+		//Button for changing the SBML file name
 		Button sbmlFileNameChangeButton = new Button("Change");
 		sbmlFileNameChangeButton.setOnAction(new EventHandler<ActionEvent>() {
 			
@@ -163,30 +203,39 @@ public class SheetProjectView {
 				gp.requestFocus();
 			}
 		});
-		
+		//HBox combining the SBML file name controls: Button & TextField
 		HBox sbmlFileNameControls = new HBox();
 		sbmlFileNameControls.getChildren().addAll(sbmlFileName, sbmlFileNameChangeButton);
 		gp.add(sbmlFileNameText, 0, 1);
 		gp.add(sbmlFileNameControls, 1, 1);
+		//VBox for all spreadsheets
 		VBox spreadsheetListing = new VBox();
-
+		//check if there are any Spreadsheets in project
 		if (project.getPaths().isEmpty()) {
+			//add one empty line if not 
 			spreadsheetListing.getChildren().add(spreadsheetLine());
 		} else {
-			boolean toggle = false;
+			//for every defined Spreadsheet add one line, the first without a deleteButton
+			boolean toggle = false;		//flag for the deleteButton; first line is without, others are with
 			for (String path : project.getPaths()) {
 				path = path.split(File.separator)[path.split(File.separator).length - 1];
 				spreadsheetListing.getChildren().add(spreadsheetLine(toggle, spreadsheetListing, path, project.getName(path)));
-				toggle = true;
+				toggle = true;		//toggle flag
 			}
 		}
+		//include the listing in a ScrollPane to enable scrolling if the listing is to large
 		ScrollPane sp = new ScrollPane(spreadsheetListing);
 		
+		//VBox for the controls of the spreadsheetListing
 		VBox spreadsheetControls = new VBox();
+		//Text
 		Text spreadsheetText = new Text("Spreadsheets:");
 		spreadsheetText.setTextAlignment(TextAlignment.RIGHT);
+		//not look like shit
 		spreadsheetText.getStyleClass().add("text-id");
+		//ButtonBar for add and new Buttons
 		ButtonBar spreadsheetControlButtonBar = new ButtonBar();
+		//Button to create a new csv
 		Button newSpreadsheetButton = new Button("new CSV");
 		newSpreadsheetButton.setOnAction(new EventHandler<ActionEvent>() {
 			
@@ -197,6 +246,7 @@ public class SheetProjectView {
 
 
 		});
+		//Add a listener to the pathsInDir ObservableList to trigger updateList every time a new csv is added
 		pathsInDir.addListener(new ListChangeListener<String>() {
 
 			@Override
@@ -204,6 +254,7 @@ public class SheetProjectView {
 				updateList(spreadsheetListing,c.getList());
 			}
 		});
+		//Button to add a Spreadsheet to the spreadsheetListing
 		Button addSpreadsheetButton = new Button("Add");
 		addSpreadsheetButton.setOnAction(new EventHandler<ActionEvent>() {
 			
@@ -213,16 +264,65 @@ public class SheetProjectView {
 			}
 		});
 		
+		//add Buttons to ButtonBar
 		spreadsheetControlButtonBar.getButtons().addAll(newSpreadsheetButton, addSpreadsheetButton);
-		
+		//Add text and ButtonBar to VBox
 		spreadsheetControls.getChildren().addAll(spreadsheetText, spreadsheetControlButtonBar);
+		//Add VBox to GridPane
 		gp.add(spreadsheetControls, 0, 2);
 		gp.add(sp, 1, 2);
+		
+		//View Done
 		return parent;
 		
 	}
+	
+	/**
+	 * Opens a {@link FileChooser} to enable the user to choose an SBML file to generate csv files an the content of .sheets
+	 * @param parent The {@link BorderPane} {@link Node} provided by Insilico
+	 */
+	private void choseSBMLfile() {
+		final FileChooser fileChooser = new FileChooser();
+		configureFileChooser(fileChooser);
+		IEclipseContext context = mWindow.getContext();
+		Window window = context.get(Window.class);
+		try {
+			File file = fileChooser.showOpenDialog(window);
+			if (!file.equals(null)) {
+				System.out.println("Yeah SBML Laden Yeah");
+			}
+		} catch (NullPointerException e) {
+			System.err.println("No File selected");
+		}
+	}
+	
+	/**
+	 * Configures the {@link FileChooser} given as parameter; sets Title
+	 * sets initial Directory as home; sets Extension filters
+	 * @param fileChooser A fileChooser; typically for the SBML file selection.
+	 */
+	private void configureFileChooser(FileChooser fileChooser) {
+		  fileChooser.setTitle("View Pictures");
+	      fileChooser.setInitialDirectory(
+	            new File(System.getProperty("user.home"))
+	    		  );
+	      fileChooser.getExtensionFilters().addAll(
+	                new FileChooser.ExtensionFilter("SBML", "*.sbml"),
+	                new FileChooser.ExtensionFilter("XML", "*.xml"),
+	                new FileChooser.ExtensionFilter("All", "*.*")
+	    		  );
+	}
 
-
+	/**
+	 * Create a spreadsheetLine represented by a {@link HBox}. Calls {@link #spreadsheetLine(boolean, VBox))} and 
+	 * further builds on it's return value. This function is used to build spreadsheetLines during instantiation of the {@link SheetProject}.
+	 * @param deleteButton Indicates if a delete Button should be added
+	 * @param spreadsheetListing The current spreadsheetListing; a {@link VBox} containing one or more {@link HBox} objects
+	 * created by {@link #spreadsheetLine()}, {@link #spreadsheetLine(boolean, VBox)} or {@link #spreadsheetLine(boolean, VBox, String, String)}.
+	 * @param path The path to be initially set in the {@link ComboBox}.
+	 * @param name The name to be initially set in the {@link TextField}.
+	 * @return A pre-filled {@link HBox} filled during instantiation.
+	 */
 	private HBox spreadsheetLine(boolean deleteButton, VBox spreadsheetListing, String path, String name) {
 		HBox spreadsheetLine = spreadsheetLine(deleteButton, spreadsheetListing);
 		for (Node child : spreadsheetLine.getChildren()) {
@@ -235,7 +335,13 @@ public class SheetProjectView {
 		
 		return spreadsheetLine;
 	}
-
+	
+	/**
+	 * Sets the lists in the {@link ComboBox} objects to be {@code list} without the entries in {@link org.insilico.sbmlsheets.core.SheetProject#getPaths()}
+	 * @param spreadsheetListing A {@link VBox} containing one or more {@link HBox} Objects as generated by {@link #spreadsheetLine()}, {@link #spreadsheetLine(boolean, VBox)}
+	 * or {@link #spreadsheetLine(boolean, VBox, String, String)}.
+	 * @param list The list of paths to be set.
+	 */
 	private void updateList(VBox spreadsheetListing, ObservableList<? extends String> list) {
 		for (Node n : spreadsheetListing.getChildren()) {
 			if(n instanceof HBox) {
@@ -251,6 +357,11 @@ public class SheetProjectView {
 		}
 	}
 
+	/**
+	 * Returns the file names of a list of paths, where the file name is the last section of the path.
+	 * @param list An {@link ObservableList} of path {@link String}s.
+	 * @return The file names of a list of paths.
+	 */
 	private ObservableList<String> getFileNames(ObservableList<String> list) {
 		ObservableList<String> fileNames = FXCollections.observableArrayList();
 		for (String path : list) {
